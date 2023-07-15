943ce8406bd3631dd1517c2360b7d15ab17a2f38
hizkiastesfaye <tesfayehizkias@gmail.com>
2023-07-15 12:49:55 +0300

    circle

--- circle.py
+++ circle.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python3
 from math import pi
+""" python code that passes the pycodestyle"""
 
 
 def Circle(r):
+    """ circle function that result the area of circle """
+
     if r < 0:
         raise ValueError("The radius should be greater than Zero")
     if type(r) not in [int, float]:
